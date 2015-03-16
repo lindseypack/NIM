b4206898070f11fd4ca2e7eff0619229f766b536
Lindsey Pack <lindseypack@users.noreply.github.com>
2015-03-16 10:14:32 -0400

    simplified code by changing some lists to dictionaries

--- code/ap/apInv.py
+++ code/ap/apInv.py
@@ -6,7 +6,7 @@ import re
 import os
 import sys
 import smtplib
-from devices.models import AP
+from devices.models import AP as AccessPoint
 
 ## This file is used to update the access point inventory data. Use the
 ## updateAccessPoints function to run the update. The function
@@ -14,103 +14,46 @@ from devices.models import AP
 ## email report on APs that are currently down or that have recovered.
 
 ## Do an snmpwalk using cmdgen from PySNMP to get data about each AP.
-def snmpwalk(mac, name, IPs, serialno, model, controllers):
+## Takes a dictionary of OIDs and a list of controller IPs.
+def snmpwalk(OIDs, controllers):
     APs = dict()
     cmdGen = cmdgen.CommandGenerator()
-
-    oids = [name, IPs, mac, serialno, model]
-
-    for i in range(len(oids)):
+    for key in OIDs:
         for controller in controllers:
             errorIndication, errorStatus, errorIndex, varBindTable = cmdGen.bulkCmd(
                 cmdgen.CommunityData('spa'),
                 cmdgen.UdpTransportTarget((controller, 161)),
-                0, 1000, oids[i]
+                0, 1000, OIDs[key]
             )
-
-            if errorIndication:
-                print(errorIndication)
-            else:
-                if errorStatus:
-                    print('%s at %s' % (
-                        errorStatus.prettyPrint(),
-                        errorIndex and varBindTable[-1][int(errorIndex)-1] or '?'
-                        )
-                    )
-                else:
-                    for varBindTableRow in varBindTable:
-                        for name, val in varBindTableRow:
-                            num = str(name)[len(mac):].strip('.')
-                            try:
-                                if len(APs[num]) == i:
-                                    APs[num].append(str(val.prettyPrint()))
-                            except:
-                                APs[num] = [str(val.prettyPrint())]
+            for varBindTableRow in varBindTable:
+                for name, val in varBindTableRow:
+                    ## make a unique identifier for each AP
+                    num = str(name)[len(OIDs["mac"]):].strip('.')
+                    try:
+                        if key not in APs[num]:
+                            APs[num][key] = str(val.prettyPrint())
+                    except:
+                        APs[num] = {key: str(val.prettyPrint())}
     return APs
-
-
-#takes a string "com" and runs the command, returning a list of AP names
-def runCommand(com):
-    args = shlex.split(com) #separates com into indv. args
-    p = subprocess.Popen(args, stdout=subprocess.PIPE) #runs command, saves stdout
-
-    #communicate() returns a tuple (stdout,stderr)
-    #but we only want stdout
-    stdout = p.communicate()[0]
-
-    #clean the data (remove un-necessary crap)
-    stdout = stdout.replace("SNMPv2-SMI::enterprises.","")
-    stdout = stdout.replace("Hex-STRING:","")
-    stdout = stdout.replace("STRING:","")
-    stdout = stdout.replace("IpAddress:","")
-    stdout = stdout.replace("\"", "")
-    stdout = stdout.replace(" ", "")
-
-    #split stdout into lines
-    stdoutLines = stdout.split("\n")
-    stdoutLines = stdoutLines[:-1] #removes last empty row
-
-    #parse stdout into list
-    names = []
-    for line in stdoutLines:
-        names.append(line.split("=")[1])
-
-    return names
-
 
 ## Add or update all access points using Django.
 def updateAccessPoints(path, AP_OIDs, controller_IPs):
-    mac_oid = AP_OIDs[0]
-    name_oid = AP_OIDs[1]
-    IP_oid = AP_OIDs[2]
-    serialno_oid = AP_OIDs[3]
-    model_oid = AP_OIDs[4]
-    status_oid = AP_OIDs[5]
-
-    AccessPoints = snmpwalk(mac_oid, name_oid, IP_oid, serialno_oid, model_oid, controller_IPs)
-    # AccessPoints = [[name, IPs, mac, serialno, model], ...]
-
-    for AccessPoint in AccessPoints:
-        serial = AccessPoints[AccessPoint][3]
-        if serial != "0":
-            name = AccessPoints[AccessPoint][0]
-            ip = AccessPoints[AccessPoint][1]
-            mac = AccessPoints[AccessPoint][2].lstrip('0').lstrip('x')
-            model = AccessPoints[AccessPoint][4]
+    APs = snmpwalk(AP_OIDs, controller_IPs)
+    for AP in APs:
+        if APs[AP]["serial"] != "0":
+            ## make a new AP object if necessary
             try:
-                update_AccessPoint = AP(serialno=serial, ip=ip)
-                update_AccessPoint.save()
+                new_AP = AccessPoint(serialno=APs[AP]["serial"], ip=APs[AP]["ip"])
+                new_AP.save()
             except:
                 pass
-
-            update_AccessPoint = AP.objects.get(serialno=serial, autoupdate=1)
-            update_AccessPoint.ip = ip
-            update_AccessPoint.mac = mac
-            update_AccessPoint.name = name
-            update_AccessPoint.model = model
-
-            update_AccessPoint.save()
-
+            ## Update the AP's data
+            update_AP = AccessPoint.objects.get(serialno=APs[AP]["serial"], autoupdate=1)
+            update_AP.ip = APs[AP]["ip"]
+            update_AP.mac = APs[AP]["mac"].lower()[2:]
+            update_AP.name = APs[AP]["name"]
+            update_AP.model = APs[AP]["model"]
+            update_AP.save()
 
 ## Get the names of all the access points which are currently up and connected to
 ## a controller. Compare to the names in the database to find the APs that are down.
@@ -125,7 +68,7 @@ def updateStatus(controller_IPs, status_oid, email):
     upAPs = []
     for cmd in AP_command:
         upAPs.extend(runCommand(cmd))
-    storedAPs = AP.objects.all()
+    storedAPs = AccessPoint.objects.all()
     downAPs = []
     recoveredAPs = []
     for ap in storedAPs:
@@ -148,6 +91,33 @@ def updateStatus(controller_IPs, status_oid, email):
         message = '\nThe following access points were down but have recovered:\n'
         subject = 'APs have recovered'
         sendEmail(message, subject, recoveredAPs, email)
+
+#takes a string "com" and runs the command, returning a list of AP names
+def runCommand(com):
+    args = shlex.split(com) #separates com into indv. args
+    p = subprocess.Popen(args, stdout=subprocess.PIPE) #runs command, saves stdout
+
+    #communicate() returns a tuple (stdout,stderr) but we only want stdout
+    stdout = p.communicate()[0]
+
+    #clean the data
+    stdout = stdout.replace("SNMPv2-SMI::enterprises.","")
+    stdout = stdout.replace("Hex-STRING:","")
+    stdout = stdout.replace("STRING:","")
+    stdout = stdout.replace("IpAddress:","")
+    stdout = stdout.replace("\"", "")
+    stdout = stdout.replace(" ", "")
+
+    #split stdout into lines
+    stdoutLines = stdout.split("\n")
+    stdoutLines = stdoutLines[:-1] #removes last empty row
+
+    #parse stdout into list
+    names = []
+    for line in stdoutLines:
+        names.append(line.split("=")[1])
+
+    return names
 
 ## Send an email report on access point status.
 def sendEmail(messageBody, subject, APs, email):
