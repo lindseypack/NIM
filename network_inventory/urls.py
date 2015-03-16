60287a576a137e28fd7afd3727918825fbd6d66e
Lindsey Pack <lindseypack@users.noreply.github.com>
2015-03-16 10:24:39 -0400

    Updated to production settings

--- network_inventory/urls.py
+++ network_inventory/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'network_inventory.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+
+    url(r'^devices/', include('devices.urls', namespace = "devices")),
     url(r'^admin/', include(admin.site.urls)),
 )
