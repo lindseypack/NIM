768699de6c36143970a701e5f00c2e4ed2220134
Lindsey Pack <lindseypack@users.noreply.github.com>
2015-03-17 14:23:17 -0400

    Added file for django url patterns

--- network_inventory/urls.py
+++ network_inventory/urls.py
@@ -6,7 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'network_inventory.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-
-    url(r'^devices/', include('devices.urls', namespace = "devices")),
+    url(r'^devices/', include('devices.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
